7436526828a82623cffb4ee20938eb544494625b
Jaqueline <jaquelinesv90@gmail.com>
2020-08-03 21:14:39 -0300

    added new package and class

---
+++ src/algorithms/reverseString/reverseString.java
@@ -0,0 +1,27 @@
+package algorithms.reverseString;
+
+// Write a function htat takes a string as input and
+// returns the string reversed.
+// ex. Given s = "hello", return "olleh".
+public class reverseString {
+	
+	public static void main(String[] args) {
+		String s = "hello";
+		System.out.println(reverse(s));
+	}
+	
+	public static String reverse(String s) {
+		
+		char[] characters = s.toCharArray();
+		int i = 0;
+		int j = s.length() -1; // because arrays are zero-based
+		
+		while( i < j){
+			char temp = characters[i];
+			characters[i++] = characters[j];
+			characters[j--] = temp;
+		}
+		return new String(characters);
+	}
+
+}
