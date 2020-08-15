bb2a78a5da9db4fd84c0f15954fc0a6c300c1a8f
Jaqueline <jaquelinesv90@gmail.com>
2020-08-15 19:01:30 -0300

    changes

--- src/algorithms/numberOfIslands/NumberOfIslands.java
+++
@@ -1,5 +0,0 @@
-package algorithms.numberOfIslands;
-
-public class NumberOfIslands {
-
-}
---
+++ src/algorithms/stringCompression/StringCompression.java
@@ -0,0 +1,15 @@
+package algorithms.stringCompression;
+
+//Give an array of characters, compress it in-place
+//The length after compression must always be smaller 
+// than or equal to the original array. Every element of the
+//array should be a character(not int) of lenght 1.
+//After you are done modifying the input array in-place,
+//return the new length of the array.
+// example input: ["a","a","b","b","c","c","c"]
+//         output: 6  "aa" is replaced by "a2"
+//					  "bb"	is replaced by "b2"
+//					  "ccc"	is replaced by "c3"
+public class StringCompression {
+
+}
