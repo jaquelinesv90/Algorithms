113e18230238a1c2766c3429ddf8bd72d389a753
Jaqueline <jaquelinesv90@gmail.com>
2020-09-07 10:52:58 -0300

    added comments

---
+++ src/algorithms/quickSort/quickSort.java
@@ -0,0 +1,20 @@
+package algorithms.quickSort;
+/*
+An Array {15,3,2,1,9,5,7,8,6}, it first divides the input array 
+into two smaller sub-arrays: the low elements and the high elements.
+1.Pick an element,called a pivot, from the array.
+2.Partioning: reorder the array so that all elements with values less than
+the pivot come before the pivot, while all elements with values greater than the pivot
+come after it(equal values can go either way).After this partioning, the pivot is in its final position.
+3.Recursively apply the above steps to the sub-array of elements with smaller values and separately to
+the sub-array of elements with greater values.*/
+
+public class quickSort {
+
+	
+	
+	
+	
+	
+	
+}
