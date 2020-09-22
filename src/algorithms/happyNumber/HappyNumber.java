01229889d3365be2243081de58f77df5c0df69db
Jaqueline <jaquelinesv90@gmail.com>
2020-09-21 23:24:43 -0300

    new class

---
+++ src/algorithms/happyNumber/HappyNumber.java
@@ -0,0 +1,18 @@
+package algorithms.happyNumber;
+
+/*
+ * Level : easy
+ * 
+ * Write an algorithm to determine if a number is "happy"
+ * 
+ * Happy number is a number defined by the following process: Starting
+ * with any positive integer, replace the number by the sum of the squares
+ * of its digits, and repeate the process until the number equals 1(where it will stay),
+ * or it loops endlessly in a cycle which does not include 1.Those numbers for which 
+ * this process ends in 1 are happy numbers.
+ * 
+ */
+
+public class HappyNumber {
+
+}
