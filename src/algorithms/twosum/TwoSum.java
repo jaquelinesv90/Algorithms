0a7911cb76a7d8920ee97fe68b363d01a4e1fd47
Jaqueline <jaquelinesv90@gmail.com>
2020-12-06 21:39:38 -0300

    new algorithm

--- src/algorithms/twosum/TwoSum.java
+++ src/algorithms/twosum/TwoSum.java
@@ -1,26 +1,36 @@
 package algorithms.twosum;
 
 /*Given an array of integers num and an integer target, return indices 
- * of the two numbers such taht they add up to target.
+ * of the two numbers such that they add up to target.
  * You may assume that each input would have exactly one solution, and
  * you may not use the same element twice
  * 
+ * input: nums = {2,7,11,15} target = 9
+ * output: {0,1}
+ * output: because  nums[0] + nums[1] == 9, return [0,1]
  */
 public class TwoSum {
 	
-	public static void main(String[] args){
-		int[] array = {2,7,11,15};
+	public static void main(String[] args) {
+		int[] nums = { 2, 7, 11, 15 };
+		int[] output = new int[2];
 		int target = 9;
-		int[] result = new int[2];
 		int number = 0;
-		for(int i =0; i< array.length; i++){
-			number = array[i];
-			if(number +array[i+1] == target){
-				break;
-			}else{
-				
+
+		for (int i = 0; i < nums.length; i++) {
+			for(int j = 0; j< nums.length; j++){
+			
+				number = nums[i];
+				if (number + nums[i + 1] == target) {
+					output[j] = nums[i];
+					break;
+	
+				}
 			}
 		}
-		
+		for(int i = 0; i < nums.length; i++){
+			System.out.println("output: "+ output[i]);
+		}
+
 	}
 }
