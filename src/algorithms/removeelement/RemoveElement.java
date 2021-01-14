583e722e11ca8da89c21ec3856b9945f66101b2f
Jaqueline <jaquelinesv90@gmail.com>
2021-01-14 18:05:40 -0300

    new class and explanation

---
+++ src/algorithms/removeelement/RemoveElement.java
@@ -0,0 +1,19 @@
+package algorithms.removeelement;
+
+/*Give an array nums and a value val, removel all instances of that value in-place
+ * and return the new length.
+ * 
+ * Do not allocate extra space for another array, you must do this by modifying the
+ * input array in-place with 0(1) extra memory.
+ * 
+ * The order of elements ca be changed.It doesn't matter what you leave beyond the
+ * new length.
+ * 
+ * ex: nums = [3,2,2,3] val = 3
+ * output 2, nums = [2,2]
+ * 
+ * 
+ */
+public class RemoveElement {
+
+}
