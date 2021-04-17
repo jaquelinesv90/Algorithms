6b55d65f0efd88317c4fd8746ff41862fe5d82ac
Jaqueline <jaquelinesv90@gmail.com>
2021-04-16 23:02:20 -0300

    new class

---
+++ src/algorithms/palindromeIndex/PalindromeIndex.java
@@ -0,0 +1,16 @@
+package algorithms.palindromeIndex;
+
+public class PalindromeIndex {
+	
+	
+	public static void main(String[] args) {
+		String s = "bcbc";
+		palindrome(s);
+	}
+	
+	public static void palindrome(String s){
+		
+		
+	}
+
+}
