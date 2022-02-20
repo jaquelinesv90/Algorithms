1e6cde574048c862899f530d1afdc637e16e4071
Jaqueline <jaquelinesv90@gmail.com>
2022-02-20 19:33:04 -0300

    new algorithm

--- src/algorithms/stringRange/StringRange.java
+++ src/algorithms/stringRange/StringRange.java
@@ -1,9 +1,11 @@
+package algorithms.stringRange;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Test {
-
+public class StringRange {
+	
 	public static void main(String[] args) {
 		int min = 1;
 		int max = 10;
@@ -25,4 +27,5 @@ public class Test {
 
 		}
 	}
+
 }
