e7f7cc842395068675f2d21068d6d5c22b54a068
Jaqueline <jaquelinesv90@gmail.com>
2022-04-17 05:55:04 -0300

    removed class 'test' and added new algorithm

--- src/algorithms/longestPalindromicSubstring/LongestPalindromicSubstring.java
+++ src/algorithms/longestPalindromicSubstring/LongestPalindromicSubstring.java
@@ -1,5 +1,20 @@
 package algorithms.longestPalindromicSubstring;
 
+/*
+ *  Given a string s, return the longest palindromic substring in s.
+ *  input: s = "babad"
+ *  output: "bab"
+ *  
+ *  input: s = "bananas"
+ *  output: "anana" 
+ */
+
 public class LongestPalindromicSubstring {
+	
+	public static void main(String[] args){
+		String s = "bananas";
+		
+		
+	}
 
 }
